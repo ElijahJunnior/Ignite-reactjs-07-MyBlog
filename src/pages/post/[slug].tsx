f865da6a514e09b187644e1311038814970370fc
Elias Junior <elijah.junnior@outlook.com>
2021-12-28 22:36:15 -0300

    back of post page created

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,16 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Header from '../../components/Header'
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+
 
 interface Post {
   first_publication_date: string | null;
@@ -26,20 +33,68 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+  return (
+    <>
+      <Header />
+      <main className={commonStyles.pageSize}>
+        <img src={post.data.banner.url} alt='banner' />
+        <h1>{post.data.title}</h1>
+        <div>
+          <time>{post.first_publication_date}</time>
+          <span>{post.data.author}</span>
+          <span>{'15 Min'}</span>
+        </div>
+        {
+          post.data.content.map((cur, ind) => (
+            <div key={ind}>
+              {cur.heading ? <h2>{cur.heading}</h2> : ''}
+              <div dangerouslySetInnerHTML={{ __html: RichText.asHtml(cur.body) }} />
+            </div>
+          ))
+        }
+      </main>
+    </>
+  )
+}
 
-//   // TODO
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+  // // TODO
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
 
-//   // TODO
-// };
+  const revalidate = 60;
+
+  try {
+
+    const prismic = getPrismicClient();
+
+    const res = await prismic.getByUID('posts', String(params.slug), {});
+
+    return {
+      props: {
+        post: res
+      },
+      revalidate
+    };
+
+  } catch (err) {
+
+    console.log(err);
+
+    return {
+      revalidate,
+      notFound: true
+    };
+
+  }
+
+};
